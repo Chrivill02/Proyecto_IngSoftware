5d4287b724c1a458cd6e1296009530d52a063a1d
David-num <124700120+David-num@users.noreply.github.com>
2025-09-27 16:52:30 -0600

    Scene

---
+++ Scenary/Assets/Scripts/PelusaMoviment.cs
@@ -0,0 +1,66 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float JumpForce;
+    public float Speed;
+    public float FuerzaRebote;
+
+    private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
+    private bool recibirDaño;
+    private float Horizontal;
+    private bool Grouded;
+
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        Animator.SetBool("running", Horizontal != 0.0f);
+
+        Debug.DrawRay(transform.position, Vector3.down * 1.5f, Color.red);
+        if (Physics2D.Raycast(transform.position, Vector3.down, 1.5f))
+        {
+            Grouded = true;
+        }
+        else Grouded = false;
+
+        if (Input.GetKeyDown(KeyCode.W) && Grouded && !recibirDaño)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.linearVelocity = new Vector2(Horizontal * Speed, Rigidbody2D.linearVelocity.y);
+    }
+
+    public void RecibirDanio(Vector2 direccion, int canDanio)
+    {
+        recibirDaño = true;
+        Vector2 rebote = new Vector2(transform.position.x - direccion.x, 1).normalized;
+        Rigidbody2D.AddForce(rebote * FuerzaRebote, ForceMode2D.Impulse);
+        
+    }
+
+    public void DesactivaDani()
+    {
+        recibirDaño = false;
+    }
+}
---
+++ Scenary/Assets/Scripts/ZonaPeligrosa.cs
@@ -0,0 +1,45 @@
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+
+public class ZonaPeligrosa : MonoBehaviour
+{
+    public Transform player;
+    public float detectionRadius;
+    public float speed;
+
+
+    private Rigidbody2D Rigidbody2D;
+    private Vector2 movement;
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToPlay = Vector2.Distance(transform.position, player.position);
+        if (distanceToPlay < detectionRadius)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            movement = new Vector2(direction.x, 0);
+        }
+        else
+        {
+            movement = Vector2.zero;
+        }
+
+        Rigidbody2D.MovePosition(Rigidbody2D.position + movement * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Vector2 direccionDanio = new Vector2(transform.position.x, 0);
+
+            collision.gameObject.GetComponent<PlayerController>().RecibirDanio(direccionDanio, 1);
+        }
+    }
+}
