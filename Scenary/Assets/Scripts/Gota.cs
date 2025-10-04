67aacadfcfccbc184ee400356092ba410c000a49
CCVela1017 <124698727+CCVela1017@users.noreply.github.com>
2025-10-04 09:19:47 -0600

    created platform additional physics and obstacle simulation

--- Scenary/Assets/Scripts/Gota.cs
+++ Scenary/Assets/Scripts/Gota.cs
@@ -5,24 +5,26 @@ public class Gota : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private Vector3 inicial;
+    private Quaternion rotacionInicial;
     void Start()
     {
         inicial = transform.position;
+        rotacionInicial = transform.rotation;
     }
 
     // Update is called once per frame
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Suelo") || collision.gameObject.tag == "Obstaculo")
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Suelo") || collision.gameObject.tag == "Obstaculo" || collision.gameObject.tag == "Player")
         {
             transform.position = inicial;
-            
+            transform.rotation = rotacionInicial;
         } 
     }
     void Update()
     {
-       
+        
        
     }
 }
