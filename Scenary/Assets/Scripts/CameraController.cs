85c22ef9ddfd546d845281314d35adea26564c6e
brandonvasm <brandonmonzon2005@gmail.com>
2025-10-11 16:31:25 -0600

    feat: improved platforms, enemies, and background interaction

---
+++ Scenary/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform objetivo;
+    public float speed = 0.025f;
+    public Vector3 desplazamiento;
+
+    private void LateUpdate()
+    {
+        if (objetivo != null)
+        {
+            Vector3 posicionDeseada = objetivo.position + desplazamiento;
+
+            Vector3 posicionSuavizada = Vector3.Lerp(transform.position, posicionDeseada, speed);
+
+            transform.position = posicionSuavizada;
+
+        }
+    }
+}
