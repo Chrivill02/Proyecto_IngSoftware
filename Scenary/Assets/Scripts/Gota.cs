bb1eaa75c26b8814033f91055188917bf9a9e465
David-num <124700120+David-num@users.noreply.github.com>
2025-10-20 18:05:42 -0600

    Agregación

---
+++ Scenary/Assets/Scripts/Gota.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Gota : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Vector3 inicial;
+    private Quaternion rotacionInicial;
+    void Start()
+    {
+        inicial = transform.position;
+        rotacionInicial = transform.rotation;
+    }
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Suelo") || collision.gameObject.tag == "Obstaculo" || collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy")
+        {
+            transform.position = inicial;
+            transform.rotation = rotacionInicial;
+        } 
+    }
+    void Update()
+    {
+        
+       
+    }
+}
